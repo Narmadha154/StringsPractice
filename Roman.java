738f5c70d0eb6baa2bf1298d214b802971707147
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-09-13 17:53:25 +0530

    Add files via upload

--- Roman.java
+++ Roman.java
@@ -3,17 +3,52 @@ package com.company.string;
 import java.util.Scanner;
 
 public class Roman {
-    /*public static int value(char r){
-
-    }*/
-    public static void convertRoman(){
+    public static int value(char r){
+       if(r=='I'){
+           return 1;
+       }
+       if(r=='V'){
+           return 5;
+       }
+        if(r=='X'){
+            return 10;
+        }
+        if(r=='L'){
+            return 50;
+        }
+        if(r=='C'){
+            return 100;
+        }
+        if(r=='D'){
+            return 500;
+        }
+        if(r=='M'){
+            return 1000;
+        }
+        return -1;
+    }
+    public static int convertRoman(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string:");
         String str = sc.nextLine();
         int n=str.length();
         int res=0;
         for(int i=0;i<n;i++){
-           // int s1=value(str.charAt(i));
+            int s1=value(str.charAt(i));
+            if(i+1<n){
+                int s2=value(str.charAt(i));
+                if(s1>=s2){
+                    res=res+s1;
+                }
+                else{
+                    res=res+s2-s1;
+                    i++;
+                }
+            }
+            else{
+                res=res+s1;
+            }
         }
+        return res;
     }
 }
