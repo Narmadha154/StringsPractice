da34554e4371b069bab17e8c947cb02c5b56a725
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-09-13 17:38:37 +0530

    Add files via upload

---
+++ Roman.java
@@ -0,0 +1,19 @@
+package com.company.string;
+
+import java.util.Scanner;
+
+public class Roman {
+    /*public static int value(char r){
+
+    }*/
+    public static void convertRoman(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String str = sc.nextLine();
+        int n=str.length();
+        int res=0;
+        for(int i=0;i<n;i++){
+           // int s1=value(str.charAt(i));
+        }
+    }
+}
